09d2a6390bc27b532d04905f466f5f5a2822e1bb
Brady Robshaw <heybrady88@gmail.com>
2022-08-26 12:45:09 -0400

    Initial Commit

---
+++ Simple dice roller/Simple_dice_rollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_dice_rollerApp.swift
+//  Simple dice roller
+//
+//  Created by Brady Robshaw on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_dice_rollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
